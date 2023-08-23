19b646d945b3f478af6670690c18b8f7f4ae677e
ivantaleika <ivan.taleika@gmail.com>
2023-08-23 23:17:59 +0200

    Rewritten app to properly use input data

--- src/coinbase/app.py
+++ src/coinbase/app.py
@@ -1,186 +1,212 @@
-import dataclasses
 import json
+import threading
+import time
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from time import sleep
+from queue import PriorityQueue
 
-import numpy as np
 import pandas as pd
-from websockets.sync.client import connect
-
-PRODUCT_ID = "BTC-USD"
-
-SUBSCRIBE_JSON = {
-    "type": "subscribe",
-    "channels": ["level2_batch"],
-    "product_ids": [PRODUCT_ID]
-}
-UNSUBSCRIBE_JSON = {
-    "type": "unsubscribe"
-}
-SUBSCRIBE_MESSAGE = json.dumps(SUBSCRIBE_JSON)
-UNSUBSCRIBE_MESSAGE = json.dumps(UNSUBSCRIBE_JSON)
-
-
-# TODO: does the "current highest" and "current lowest" refer to the values observed from the beginning of the execution, or during the last update?
-# TODO make dataclasses private to the CoinbaseFeedAnalyzer
-# TODO: work on method names
-
-@dataclass(frozen=True)
-class FeedUpdateStats:
-    time: pd.Timestamp
-    lowest_asks: pd.DataFrame
-    highest_bids: pd.DataFrame
-    lowest_ask_price: float
-    lowest_ask_quantity: float
-    highest_bid_price: float
-    highest_bid_quantity: float
-
-    @classmethod
-    def from_feed(cls, asks_update: pd.DataFrame, bids_update: pd.DataFrame,
-                  update_time: pd.Timestamp):
-        lowest_asks = asks_update[(asks_update["price_level"] == asks_update["price_level"].min())]
-        lowest_asks["time"] = update_time
-        highest_bids = bids_update[(bids_update["price_level"] == bids_update["price_level"].max())]
-        highest_bids["time"] = update_time
-
-        lowest_ask_price = lowest_asks["price_level"].values[0] if not lowest_asks.empty else np.nan
-        lowest_ask_quantity = lowest_asks["quantity"].values[0] if not lowest_asks.empty else np.nan
-
-        highest_bid_price = highest_bids["price_level"].values[0] if not highest_bids.empty else np.nan
-        highest_bid_quantity = highest_bids["quantity"].values[0] if not highest_bids.empty else np.nan
-
-        return cls(update_time, lowest_asks, highest_bids, lowest_ask_price, lowest_ask_quantity,
-                   highest_bid_price, highest_bid_quantity)
+import websocket
 
 
 @dataclass(frozen=True)
-class FeedState:
-    time: pd.Timestamp
-    lowest_asks: pd.DataFrame
-    highest_bids: pd.DataFrame
-    lowest_ask_price: float
-    lowest_ask_quantity: float
-    highest_bid_price: float
-    highest_bid_quantity: float
-
-    @classmethod
-    def from_update_stats(cls, stats: FeedUpdateStats) -> "FeedState":
-        return cls(stats.time, stats.lowest_asks, stats.highest_bids, stats.lowest_ask_price,
-                   stats.lowest_ask_quantity, stats.highest_bid_price,
-                   stats.highest_bid_quantity)
-
-    def drop_history(self, before: pd.Timestamp) -> "FeedState":
-        # TODO: does pandas know that it is sorted?
-        return dataclasses.replace(
-            self,
-            lowest_asks=self.lowest_asks[self.lowest_asks["time"] > before],
-            highest_bids=self.highest_bids[self.highest_bids["time"] > before]
-        )
-
-    def update(self, stats: FeedUpdateStats) -> "FeedState":
-        lowest_asks = pd.concat([self.lowest_asks, stats.lowest_asks])
-        highest_bids = pd.concat([self.highest_bids, stats.highest_bids])
-
-        if stats.lowest_ask_price < self.lowest_ask_price:
-            lowest_ask_price = stats.lowest_ask_price
-            lowest_ask_quantity = stats.lowest_ask_quantity
-        else:
-            lowest_ask_price = self.lowest_ask_price
-            lowest_ask_quantity = self.lowest_ask_quantity
-
-        if stats.highest_bid_price > self.highest_bid_price:
-            highest_bid_price = stats.highest_bid_price
-            highest_bid_quantity = stats.highest_bid_quantity
-        else:
-            highest_bid_price = self.highest_bid_price
-            highest_bid_quantity = self.highest_bid_quantity
-        return FeedState(
-            stats.time,
-            lowest_asks,
-            highest_bids,
-            lowest_ask_price,
-            lowest_ask_quantity,
-            highest_bid_price,
-            highest_bid_quantity
-        )
+class Operation(ABC):
+    price_level: float
+    quantity: float
 
     @property
-    def highest_bid_ask_diff(self) -> float:
-        return self.highest_bid_price - self.lowest_ask_price
+    @abstractmethod
+    def priority(self):
+        pass
 
-    def __str__(self):
-        return f"State for {self.time}:\n" \
-               f"  Highest bid: price - {self.highest_bid_price}, quantity - {self.highest_bid_quantity}\n" \
-               f"  Lowest ask: price - {self.lowest_ask_price}, quantity - {self.lowest_ask_quantity}\n" \
-               f"  Biggest ask and bid price difference: price - {self.highest_bid_ask_diff}."
+    def __lt__(self, other):
+        return self.priority < other.priority
 
 
-class CoinbaseFeedAnalyzer:
-    DEFAULT_AGGREGATION_WINDOWS_MINUTES = [1, 5, 15]
+class Bid(Operation):
+    @property
+    def priority(self):
+        return -self.price_level
 
-    def __init__(self, snapshot: dict, product_id: str, aggregation_windows_minutes=None):
-        if aggregation_windows_minutes is None:
-            aggregation_windows_minutes = self.DEFAULT_AGGREGATION_WINDOWS_MINUTES
-            aggregation_windows_minutes.sort()
-        self.aggregation_windows = [pd.Timedelta(minutes=m) for m in aggregation_windows_minutes]
-        self.product_id = product_id
 
-        snapshot_asks = pd.DataFrame(snapshot["asks"], columns=["price_level", "quantity"], dtype=float)
-        snapshot_bids = pd.DataFrame(snapshot["bids"], columns=["price_level", "quantity"], dtype=float)
-        # TODO: time from the response does not reflect that 5 seconds passed
-        snapshot_time = pd.to_datetime(snapshot["time"])
-        feed_update_stats = FeedUpdateStats.from_feed(snapshot_asks, snapshot_bids, snapshot_time)
-        self.feed_state = FeedState.from_update_stats(feed_update_stats)
-        self.mid_prices = self.__calc_mid_prices()
+class Ask(Operation):
+    @property
+    def priority(self):
+        return self.price_level
 
-    def __calc_mid_prices(self):
-        # TODO: move to somewhere?
-        mid_prices = {}
-        for window in self.aggregation_windows:
-            window_lowest_asks = self.feed_state.lowest_asks[
-                self.feed_state.lowest_asks["time"] > self.feed_state.time - window]
-            window_highest_bids = self.feed_state.highest_bids[
-                self.feed_state.highest_bids["time"] > self.feed_state.time - window]
-            window_lowest_ask_price = window_lowest_asks["price_level"].min()
-            window_highest_bid_price = window_highest_bids["price_level"].max()
-            mid_price = (window_lowest_ask_price + window_highest_bid_price) / 2
-            mid_prices[f"{window.seconds / 60} minute(s)"] = mid_price
-        return mid_prices
+
+@dataclass(frozen=True)
+class OperationsDiff:
+    bid: Bid
+    ask: Ask
+    # TODO: which timestamp type to use?
+    observed_at: str
+
+    @property
+    def diff(self):
+        return self.ask.price_level - self.bid.price_level
+
+    def __lt__(self, other):
+        return self.diff < other.diff
+
+    def __eq__(self, other):
+        return self.diff == other.diff
+
+    def __gt__(self, other):
+        return self.diff > other.diff
+
+
+class OrderBook:
+    __ZERO_QUANTITY_THRESHOLD = 0.1
+
+    def __init__(self, snapshot: dict):
+        self.bids: PriorityQueue[Bid] = PriorityQueue()
+        self.asks: PriorityQueue[Ask] = PriorityQueue()
+        self.n_bids_zeros = 0
+        self.n_asks_zeros = 0
+
+        snapshot_time = snapshot['time']
+
+        for price_level_s, quantity_s in snapshot['bids']:
+            self.__insert_record('buy', float(price_level_s), float(quantity_s))
+
+        for price_level_s, quantity_s in snapshot['asks']:
+            self.__insert_record('sell', float(price_level_s), float(quantity_s))
+
+        self.__compact()
+        self.operations_diff = OperationsDiff(self.__get_first_record('buy'), self.__get_first_record('sell'), snapshot_time)
 
     def update(self, update: dict):
-        update_df = pd.DataFrame(update["changes"], columns=["side", "price_level", "quantity"])
-        update_time = pd.to_datetime(update["time"])
-        update_asks = update_df[update_df["side"] == "sell"].drop(columns="side").astype(float)
-        update_bids = update_df[update_df["side"] == "buy"].drop(columns="side").astype(float)
-        feed_update_stats = FeedUpdateStats.from_feed(update_asks, update_bids, update_time)
-        self.feed_state = self.feed_state.drop_history(update_time - self.aggregation_windows[-1])
-        self.feed_state = self.feed_state.update(feed_update_stats)
-        self.mid_prices = self.__calc_mid_prices()
+        update_time = update['time']
+
+        for side, price_level_s, quantity_s in update['changes']:
+            self.__insert_record('buy', float(price_level_s), float(quantity_s))
+        self.__compact()
+
+        new_operations_diff = OperationsDiff(self.__get_first_record('buy'), self.__get_first_record('sell'), update_time)
+        self.operations_diff = new_operations_diff if new_operations_diff > self.operations_diff else self.operations_diff
+
+    def __get_first_record(self, side) -> Operation:
+        if side == 'buy':
+            pq = self.bids
+        else:
+            pq = self.asks
+        while not pq.empty():
+            record = pq.queue[0]
+            if record.quantity > 0:
+                return record
+            else:
+                pq.get()
+                self.__change_n_zeros(side, -1)
+        else:
+            raise ValueError(f"Operation book for '{side}'s is empty")
+
+    def __insert_record(self, side, price_level, quantity):
+        if side == 'buy':
+            self.bids.put(Bid(price_level, quantity))
+        else:
+            self.asks.put(Ask(price_level, quantity))
+
+        if quantity == 0:
+            self.__change_n_zeros(side, 1)
+
+    def __change_n_zeros(self, side: str, by: int):
+        if side == 'buy':
+            self.n_bids_zeros += by
+        else:
+            self.n_asks_zeros += by
+
+    def __compact(self):
+        # Algorithms theory describes an IndexPriorityQueue (for example,
+        # https://algs4.cs.princeton.edu/24pq/IndexMinPQ.java.html) with O(log(n)) deletion. We can define max
+        # precision for the price, multiply it by 10^precision and use it as an index. Then we can delete 0 quantity
+        # orders in O(log(n)) time. However, the only implementation for python I was able to find
+        # (https://github.com/nvictus/priority-queue-dictionary) isn't widely used. While it can be OK for PoC,
+        # I won't use such a solution in production. That is why we keep zeros in the queue and compact it from time
+        # to time instead.
+        self.n_bids_zeros, self.bids = self.__compact_queue(self.n_bids_zeros, self.bids)
+        self.n_asks_zeros, self.asks = self.__compact_queue(self.n_asks_zeros, self.asks)
+
+    # TODO: change to static?
+    def __compact_queue(self, n_zeros: int, pq: PriorityQueue[Operation]) -> (int, PriorityQueue[Operation]):
+        if n_zeros / pq.qsize() > self.__ZERO_QUANTITY_THRESHOLD:
+            new_pq = PriorityQueue()
+            # The overall complexity is O(m*log(m)). Additionally, the source queue is already partially sorted,
+            # reducing number of swim operations we need to insert a record into the new queue.
+            for operation in pq.queue:
+                if operation.quantity > 0:
+                    new_pq.put(operation)
+            return 0, new_pq
+        else:
+            return n_zeros, pq
 
     def print_stats(self):
-        print(f"Product {self.product_id}. {self.feed_state}")
-        print(f"Mid prices: {self.mid_prices}")
+        highest_bid = self.__get_first_record('buy')
+        lowest_ask = self.__get_first_record('sell')
+        print(f"Highest bid: price - {highest_bid.price_level}, quantity - {highest_bid.quantity}")
+        print(f"Lowest ask: price - {lowest_ask.price_level}, quantity - {lowest_ask.quantity}")
+
+        print(f"Biggest difference in price between the highest bid and lowest ask we have seen so far is "
+              f"{self.operations_diff.diff}, observed at {self.operations_diff.observed_at}.")
 
 
-with connect("wss://ws-feed.exchange.coinbase.com") as websocket:
-    websocket.send(SUBSCRIBE_MESSAGE)
-    subscription_confirmation = json.loads(websocket.recv())
-    if subscription_confirmation["type"] == "error":
-        # TODO: add logging/printing
-        raise Exception(f"Subscription error: {subscription_confirmation}")
-    else:
-        print("Subscription confirmed.")
-    try:
-        feed_snapshot = json.loads(websocket.recv())
-        analyzer = CoinbaseFeedAnalyzer(feed_snapshot, PRODUCT_ID)
-        analyzer.print_stats()
+class OrderBookApp:
+    def __init__(self, product_id: str):
+        self.product_id = product_id
+        self.websocket = None
+        self.order_book = None
+        self.websocket_thread = None
+
+    def __enter__(self):
+        self.websocket = websocket.WebSocketApp(ws_url,
+                                                on_open=self.__on_open,
+                                                on_message=self.__on_message,
+                                                on_error=self.__on_error,
+                                                on_close=self.__on_close)
+        self.websocket_thread = threading.Thread(target=self.websocket.run_forever)
+        self.websocket_thread.start()
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # TODO: not sure if this implementation is correct
+        # TODO: send exit message to the websocket
+        self.websocket.close()
+        self.websocket_thread.join()
+
+    def print_stats(self):
+        # TODO: don't use pandas?
+        current_time = pd.to_datetime('now')
+        print(f"Order book stats for {self.product_id} at {current_time}:")
+        self.order_book.print_stats()
+
+    def __on_open(self, ws):
+        subscribe_message = {
+            "type": "subscribe",
+            "channels": [{"name": "level2_batch", "product_ids": [self.product_id]}]
+        }
+        ws.send(json.dumps(subscribe_message))
+
+    def __on_message(self, _, message):
+        data = json.loads(message)
+        if data['type'] == 'snapshot':
+            self.order_book = OrderBook(data)
+        elif data['type'] == 'l2update':
+            self.order_book.update(data)
+        else:
+            raise ValueError(f"Unknown message is received: {message}")
+
+    def __on_error(self, ws, error):
+        print(f"Error: {error}")
+
+    def __on_close(self, ws, close_status_code, close_msg):
+        print("Closed")
+
+
+if __name__ == "__main__":
+    ws_url = "wss://ws-feed.pro.coinbase.com"
+    product_id = "BTC-USD"
+    with OrderBookApp(product_id) as app:
         while True:
-            sleep(5)
-            update = json.loads(websocket.recv())
-            analyzer.update(update)
-            analyzer.print_stats()
-    finally:
-        # TODO: unsubscribe doesn't return expected response, but simply sends the next update
-        websocket.send(UNSUBSCRIBE_MESSAGE)
-        # unsubscribe_confirmation = json.loads(websocket.recv())
-        # print(f"Unsubscription confirmation {unsubscribe_confirmation}")
+            time.sleep(5)
+            app.print_stats()
